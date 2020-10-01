8538c8548e19a680686c70513d6244ee574011cf
Xavier Linn <xperrylinn@berkeley.edu>
2020-10-01 12:09:05 -0700

    getting the datat i want from leaderboard

--- prototype_selenium_scrape_leaderboard.py
+++ prototype_selenium_scrape_leaderboard.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import os
+import time
 
 driver = webdriver.Chrome('./chromedriver')
 
@@ -18,12 +19,15 @@ password.send_keys(os.environ['STRAVA_PASSWORD'])
 login_attempt = driver.find_element_by_xpath("//*[@type='submit']")
 login_attempt.submit()
 
+time.sleep(5)
+
 driver.get("https://www.strava.com/segments/902554?filter=overall")
 print('driver.current_url: ', driver.current_url)
 
-element = driver.find_element_by_id('segment-leaderboard')
+# element = driver.find_element_by_id('segment-results')
+element = driver.find_element_by_xpath('//div[@data-tracking]')
 
-print(element)
+print(element.get_attribute('data-tracking'))
 
 # try:
 #     elements = wait.until(
@@ -36,9 +40,5 @@ print(element)
 #     )
 # finally:
 #     driver.close()
-
-for element in elements:
-    print(element)
-elements.clear()
 
 driver.close()
